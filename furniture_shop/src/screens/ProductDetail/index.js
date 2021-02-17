9bda0b87dafb893b25ac13480f90be3398467400
Princewill Iroka <princewilliroka@outlook.com>
2021-02-17 12:30:06 +0100

    Added furniture_shop app

---
+++ furniture_shop/src/screens/ProductDetail/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function ProductDetail() {
+  return (
+    <View>
+      <Text>ProductDetail</Text>
+    </View>
+  );
+}
